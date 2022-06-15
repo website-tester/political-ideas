6440c66cd3fa1892825afe5f21cb0cbfd7428fe4
website-tester <70786799+website-tester@users.noreply.github.com>
2022-06-15 14:58:33 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,19 @@
 
 const lmnt = selector => document.querySelector(selector),
 lmnts = (...selectors) => [...document.querySelectorAll(selectors)]
+
+const nav_bar = lmnt(`#nav_bar`),
+btn_nav = lmnt(`#btn_nav`)
+
+let nav_default = nav_bar.style.display || `block`
+if (nav_default == ``) nav_default = `block`
+
+function hide_show_nav () {
+	console.log(`hide_show_nav running`)
+	if (nav_bar.style.display == `none`) 
+		nav_bar.style.display = nav_default
+	else nav_bar.style.display = `none`
+}
+
+if (btn_nav.onclick=="hide_show_nav()") console.log("yes")
+else console.log(btn_nav.onclick)
