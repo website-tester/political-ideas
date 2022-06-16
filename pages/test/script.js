0a7f6b87637747ff4efb5e53d179734e8f8c9816
website-tester <70786799+website-tester@users.noreply.github.com>
2022-06-16 23:29:05 +0800

    Add files via upload

---
+++ pages/test/script.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const lmnt = selector => document.querySelector(selector),
+lmnts = (...selectors) => [...document.querySelectorAll(selectors)]
+
+const body = lmnt(`body`), 
+header = lmnt(`header`),
+main = lmnt(`main`),
+nav_bar = lmnt(`#nav_bar`),
+main_sections = lmnts(`main section`),
+main_top = lmnt(`#main_top`)
+
+main.style.top = header.clientHeight+'px'
+//main.style.padding = "0px 3ch"
+
+
+let set_width = (+document.body.clientWidth)+`px`
+
+function set_max_widths () {
+	set_width = (+document.body.clientWidth)+`px`
+
+	nav_bar.style.width = main.style.width = header.style.width =
+	nav_bar.style.maxWidth = main.style.maxWidth = header.style.maxWidth = set_width
+	main_sections.forEach(section => section.maxWidth = (+document.body.clientWidth-10)+`px`)
+
+	if (header.clientHeight <991) main.classList.add('mobile_resizer')
+	else main.classList.remove('mobile_resizer')
+}
+set_max_widths ()
+window.onresize = set_max_widths
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  const header_minus_nav_height = header.clientHeight - nav_bar.clientHeight
+  if (prevScrollpos > currentScrollPos) {
+    header.style.top = -header_minus_nav_height+"px"// "-52px";
+  } else {
+    header.style.top = -header.clientHeight+"px" //"-123px";
+  }
+  prevScrollpos = currentScrollPos;
+  if (window.scrollY==0) {
+	header.style.top = "0px";
+  }
+}
